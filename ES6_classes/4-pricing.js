41317ba009ddc1807f41661d55401ca941bd3b5a
GaboAfk <gabrielmorffe95@gmail.com>
2024-07-26 16:19:15 -0300

    validators added

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,9 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+
 import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this._amount = this._validatorAmount(amount);
+    this._currency = this._validatorCurrency(currency);
+  }
+
+  _validatorAmount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    return amount;
+  }
+
+  _validatorCurrency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be an instance of "Currency" class');
+    }
+    return currency;
   }
 
   get amount() {
@@ -15,15 +32,15 @@ export default class Pricing {
   }
 
   set amount(newamount) {
-    this._amount = newamount;
+    this._amount = this._validatorAmount(newamount);
   }
 
   set currency(newcurrency) {
-    this._currency = newcurrency;
+    this._currency = this._validatorCurrency(newcurrency);
   }
 
   displayFullPrice() {
-    const fullcurrency = Currency.displayFullCurrency();
+    const fullcurrency = this._currency.displayFullCurrency();
     return `${this._amount} ${fullcurrency}`;
   }
 
