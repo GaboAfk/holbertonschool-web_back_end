15d48828019a118d1981171e8ed61442ef8b19fc
GaboAfk <gabrielmorffe95@gmail.com>
2024-07-30 16:15:13 -0300

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,9 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((totalRes) => (totalRes.map((singleRes) => ({
+    .then((totalRes) => (totalRes.forEach((singleRes) => ({
       status: singleRes.status,
-      value: singleRes.status === 'fulfilled' ? singleRes.status : String(singleRes.reason),
+      value: singleRes.status === 'fulfilled' ? singleRes.value : String(singleRes.reason),
     }))));
 }
 /* export default function handleProfileSignup(firstName, lastName, fileName) {
