67afae23801646c863210b8221ccd397853b8d63
GaboAfk <gabrielmorffe95@gmail.com>
2024-07-30 15:52:48 -0300

    async await Promise

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((totalRes) => (
-      totalRes.forEach((singleRes) => ({
-        status: singleRes.status,
-        value: singleRes.status === 'fulfilled' ? singleRes.value : String(singleRes.reason),
-      }))
-    ));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const totalRes = await Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return (
+    totalRes.forEach((singleRes) => ({
+      status: singleRes.status,
+      value: singleRes.status === 'fulfilled' ? singleRes.value : String(singleRes.reason),
+    }))
+  );
 }
 
 /* export default function handleProfileSignup(firstName, lastName, fileName) {
