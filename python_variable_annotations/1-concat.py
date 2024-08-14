13978f3ca4d3e72e127fff78421506bd2d537868
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-14 17:50:05 -0300

    concat str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""concat module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat two str
+
+    Args:
+        str1 (str): some str
+        str2 (str): some str
+
+    Returns:
+        str: stings concated
+    """
+    return str1 + str2
