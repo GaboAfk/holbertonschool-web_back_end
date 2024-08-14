7902e2a4d37e9222fbcafdf68f1956a941e92007
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-14 18:00:03 -0300

    floor float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""floor module"""
+
+def floor(n: float) -> int:
+    """gives n truncated
+
+    Args:
+        n (float): some float number
+
+    Returns:
+        int: n truncated floor
+    """
+    return int(n)
