db89c11ffc15f5877a0fa144408b98c46dff7148
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-14 17:45:40 -0300

    doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,2 +1,15 @@
+#!/usr/bin/env python3
+"""add module"""
+
+
 def add(a: float, b: float) -> float:
+    """sum floats
+
+    Args:
+        a (float): some float
+        b (float): some float
+
+    Returns:
+        float: float sum
+    """
     return a + b
