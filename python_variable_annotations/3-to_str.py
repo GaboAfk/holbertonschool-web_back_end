471a1a02fea17286f6ba251cb402b654afc064f4
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-14 18:06:10 -0300

    to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""to_str module"""
+
+
+def to_str(n: float) -> str:
+    """float to string
+
+    Args:
+        n (float): some float number
+
+    Returns:
+        str: string representation of n
+    """
+    return str(n)
