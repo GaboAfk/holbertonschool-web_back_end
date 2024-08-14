f4a61b57383f43eefedbae652b1c9f7349ac06ab
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-14 19:02:16 -0300

    make and calleable func

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""make multiplier module"""
+
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returns a funtion that multiplies its input by a multiplier
+
+    Args:
+        multiplier (float): an multiplier to be used in returned funct
+
+    Returns:
+        Callable[[float], float]: a funtion that takes a float and return 
+        the result of multiplies its value
+    """
+    def floatmultiplier(value: float) -> float:
+        return multiplier*value
+
+    return floatmultiplier
