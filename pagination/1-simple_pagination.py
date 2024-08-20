7f08669c6c7b1638448a28697f39bfea047f15d9
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-20 02:58:55 -0300

    copy index_range code

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
-index_range = __import__('0-simple_helper_function').index_range
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """calculate and return a size of 2 tuple with start and end range of
+      index's page
+
+    Args:
+        page (int): selected page
+        page_size (int): size of page to show
+
+    Returns:
+        Tuple[int, int]: tuple with start and end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
