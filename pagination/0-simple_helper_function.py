d95e6dfcfdcf2c8eab0578d68f406cf5fca46efc
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-20 02:17:02 -0300

    return without ()

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """simple index range module"""
-
-
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """calculate and return a tuple with start and end index of a page
+    """calculate and return a size of 2 tuple with start and end range of
+      index's page
 
     Args:
         page (int): selected page
@@ -17,4 +16,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = start_index - page_size
-    return (start_index, end_index)
+    return start_index, end_index
