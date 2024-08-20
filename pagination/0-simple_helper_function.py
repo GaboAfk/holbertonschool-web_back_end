24692c56e02ff32aa843fc40c6ffdbb636ff9a73
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-20 02:12:16 -0300

    simple index range

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""simple index range module"""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """calculate and return a tuple with start and end index of a page
+
+    Args:
+        page (int): selected page
+        page_size (int): size of page to show
+
+    Returns:
+        Tuple[int, int]: tuple with start and end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index - page_size
+    return (start_index, end_index)
