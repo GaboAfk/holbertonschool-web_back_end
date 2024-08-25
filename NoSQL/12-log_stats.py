71ebbf48a7b19c24c49e2b3cfc8467b07afea7c1
GaboAfk <gabrielmorffe95@gmail.com>
2024-08-25 01:44:21 -0300

    function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,16 +4,25 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_coll = client.logs.nginx
+def log_stats(mongo_collection):
+    """provide logs stats of nginx collection saved in a
+    mongoDB using a list of methods
 
-all_logs = nginx_coll.count_documents({})
-path_status = nginx_coll.count_documents({"path": "/status"})
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-nginx_counter = nginx_coll.count_documents
+    Args:
+        mongo_collection (Collection): mongo collection
+    """
+    all_logs = mongo_collection.count_documents({})
+    path_status = mongo_collection.count_documents({"path": "/status"})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    nginx_counter = mongo_collection.count_documents
 
-print(all_logs, 'logs')
-print('Methods:')
-for method in methods:
-    print(f"\tmethod {method}:", nginx_counter({"method": method}))
-print(path_status, "status check")
+    print(all_logs, 'logs')
+    print('Methods:')
+    for method in methods:
+        print(f"\tmethod {method}:", nginx_counter({"method": method}))
+    print(path_status, "status check")
+
+
+if __name__ == "__main__":
+    nginx_coll = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_coll)
